5c89d613a56182ec1c3d4459e7cd55a4264d5feb
Jaka Hudoklin <jakahudoklin@gmail.com>
2014-09-05 13:19:45 +0200

    update autodocker config

--- nix/autodocker.nix
+++ nix/autodocker.nix
@@ -6,12 +6,26 @@
 
 let
   update_script = ''
-    export $(xargs --null --max-args=1 echo < /proc/1/environ | grep channel)
-    export $(xargs --null --max-args=1 echo < /proc/1/environ | grep package)
+    eval $(xargs --null --max-args=1 echo < /proc/1/environ | grep channel)
+    eval $(xargs --null --max-args=1 echo < /proc/1/environ | grep package)
     export HOME=/root
-    nix-channel add $channel deploy
+    export SYSTEM_PROFILE=$(readlink -f /nix/var/nix/profiles/system)
+
+    if [[ -z "$channel" || -z "$package" ]]; then
+      echo "Channel or package not set"
+      exit 0
+    fi
+ 
+    nix-channel --list | grep deploy || nix-channel --add $channel deploy
     nix-channel --update deploy
-    nix-env -i $package
+    nix-env -iA deploy.$package -p /nix/var/nix/profiles/system
+
+    if [ "$SYSTEM_PROFILE" != "$(readlink -f /nix/var/nix/profiles/system)" ]; then
+      echo "switching config to $(readlink -f /nix/var/nix/profiles/system)"
+      /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+    else
+      echo "still on $SYSTEM_PROFILE"
+    fi
   '';
 
 in {
@@ -23,14 +37,15 @@ in {
     description = "Auto update docker container";
     script = update_script;
     path = [ pkgs.nix ];
-    after = [ "network.target "];
+    after = [ "network.target" ];
     wantedBy = ["multi-user.target"];
   };
 
   systemd.timers.autoupdate = {
     timerConfig = {
-      OnUnitInactiveSec = "120s";
+      OnUnitInactiveSec = "60s";
       Unit = "autoupdate.service";
     };
+    wantedBy = ["autoupdate.service"];
   };
 }
