b2ed688925f52180dba1449c4033cddbd2d81be8
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-01-16 19:10:40 +0100

    profiles/desktop: fix typo

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -15,7 +15,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    profile.work.enable = mkDefault true;
+    profiles.work.enable = mkDefault true;
     profiles.x11.enable = mkDefault true;
 
     # dbus
