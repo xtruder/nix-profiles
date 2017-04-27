37bfbe6773ff55f1396f1629e3e427972102e748
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-04-27 17:05:17 +0200

    modules: server role, fix typo

--- modules/roles/server.nix
+++ modules/roles/server.nix
@@ -5,7 +5,7 @@ with lib;
 {
   options.roles.server.enable = mkEnableOption "admin role";
 
-  config = mkIf config.roles.admin.enable {
+  config = mkIf config.roles.server.enable {
     roles.system.enable = mkDefault true;
 
     networking.firewall.allowedTCPPorts = [22];
