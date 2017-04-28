979e522b328c0baef6b0b0f2f3f8b7f46ce435f5
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-04-28 23:18:23 +0200

    roles/server: enable openssl

--- modules/roles/server.nix
+++ modules/roles/server.nix
@@ -3,10 +3,12 @@
 with lib;
 
 {
-  options.roles.server.enable = mkEnableOption "admin role";
+  options.roles.server.enable = mkEnableOption "server role";
 
   config = mkIf config.roles.server.enable {
     roles.system.enable = mkDefault true;
+
+    services.openssh.enable = true;
 
     networking.firewall.allowedTCPPorts = [22];
   };
