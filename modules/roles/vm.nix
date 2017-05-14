d0590b0fa49f0a3ce69dd2fab0520bc18e914699
Jaka Hudoklin <jakahudoklin@gmail.com>
2017-05-14 22:24:22 +0200

    roles/vm: switch from xpra to i3

--- modules/roles/vm.nix
+++ modules/roles/vm.nix
@@ -6,9 +6,9 @@ with lib;
   options.roles.vm.enable = mkEnableOption "vm role";
 
   config = mkIf config.roles.vm.enable {
+    profiles.i3.enable = true;
     roles.work.enable = true;
     roles.system.enable = true;
-    roles.headless.enable = true;
 
     # eth0 is by default external interface
     networking.nat.externalInterface = mkDefault "eth0";
