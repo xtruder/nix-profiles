ab446e8e34f44b6b0e6ba482fa2b4ba20a5fe592
Jaka Hudoklin <jakahudoklin@gmail.com>
2018-08-18 23:44:45 +0200

    fix(profiles,docker): fix dns

--- modules/profiles/docker.nix
+++ modules/profiles/docker.nix
@@ -9,6 +9,7 @@ in {
 
   config = mkIf config.profiles.docker.enable {
     virtualisation.docker.enable = true;
+    virtualisation.docker.extraOptions = "--dns 172.17.0.1";
 
     users.groups.docker.members = ["${config.users.users.admin.name}"];    
     environment.systemPackages = with pkgs; [
