313e9e3403583e16f0fad96e22cc0ba2ed79b771
Jaka Hudoklin <jakahudoklin@gmail.com>
2019-01-12 20:54:23 +0100

    fix(profiles/docker): fix docker-compose package name

--- modules/profiles/docker.nix
+++ modules/profiles/docker.nix
@@ -14,7 +14,7 @@ in {
     users.groups.docker.members = ["${config.users.users.admin.name}"];    
     environment.systemPackages = with pkgs; [
       docker
-      pythonPackages.docker_compose
+      docker-compose
     ];
   };
 }
