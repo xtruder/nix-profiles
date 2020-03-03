59587e3335235ef775d396fc3d31350ebcb8b00c
Jaka Hudoklin <jaka@x-truder.net>
2020-03-03 11:28:20 +0700

    nixos/hw/hyperv-vm: set attributes.isVM to true

--- modules/nixos/hw/hyperv-vm.nix
+++ modules/nixos/hw/hyperv-vm.nix
@@ -3,8 +3,5 @@
 {
   virtualisation.hypervGuest.enable = true;
 
-  services.xserver = {
-    modules = [ pkgs.xorg.xf86videofbdev ];
-    videoDrivers = [ "hyperv_fb" ];
-  };
+  attributes.isVM = true;
 }
