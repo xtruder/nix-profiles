4d6477eb51dab16c6b763aef6456a535d6594327
Jaka Hudoklin <jaka@x-truder.net>
2020-03-14 11:10:05 +0000

    home-manager/themes/i3lock: fix colors

--- modules/home-manager/themes/defaults/i3lock.nix
+++ modules/home-manager/themes/defaults/i3lock.nix
@@ -1,20 +1,24 @@
-{ config, ... }:
+{ config, lib, ... }:
+
+with lib;
 
 let
   thm = config.themes.colors;
 
+  mkColor = c: "00" + (substring 1 8 c);
+
 in {
   config = {
     programs.i3lock.colors = {
-      inside = thm.blue;
-      insideVerify = thm.green;
-      insideWrong = thm.red;
-      ringVerify = thm.dark;
-      ringWrong = thm.dark;
-      ring = thm.gray;
-      line = thm.fg;
-      keyHold = thm.blue;
-      backspaceHold = thm.blue;
+      inside = mkColor thm.blue;
+      insideVerify = mkColor thm.green;
+      insideWrong = mkColor thm.red;
+      ringVerify = mkColor thm.dark;
+      ringWrong = mkColor thm.dark;
+      ring = mkColor thm.gray;
+      line = mkColor thm.fg;
+      keyHold = mkColor thm.blue;
+      backspaceHold = mkColor thm.blue;
     };
   };
 }
