348b512bc7f88e78da2e70f2b1eeaa6ac194bf5b
Jaka Hudoklin <jaka@x-truder.net>
2020-03-18 06:18:55 +0000

    nixos/profiles/pulseaudio: allow connections from localhost

--- nixos/profiles/pulseaudio.nix
+++ nixos/profiles/pulseaudio.nix
@@ -8,6 +8,7 @@
       package = pkgs.pulseaudioFull;
       extraConfig = ''
         load-module module-switch-on-connect
+        load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
       '';
       support32Bit = true;
     };
