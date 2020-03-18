ca068ef917cdd36419ca797e8b1b27eca09ca974
Jaka Hudoklin <jaka@x-truder.net>
2020-03-18 03:38:06 +0000

    home-manager/system/env: fix typo

--- modules/home-manager/system/env.nix
+++ modules/home-manager/system/env.nix
@@ -8,7 +8,7 @@
     programs.i3status.order = [];
 
     # place default i3 bar on top
-    xsession.windowManager.i3.defaultBar.position = "top";
+    xsession.windowManager.i3.defaultBarConfig.position = "top";
 
     # disable screen locker in environments
     services.screen-locker.enable = false;
