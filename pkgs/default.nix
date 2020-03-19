9f39a000e58336364363b01e5796632005a41727
Jaka Hudoklin <jaka@x-truder.net>
2020-03-19 17:51:26 +0700

    pkgs: remove old default.nix file

--- pkgs/default.nix
+++
@@ -1,6 +0,0 @@
-super: self: {
-  nix-firefox-addons-generator = super.haskellPackages.callPackage ./firefox-addons-generator { };
-  firefox-addons = import ./firefox-addons { inherit (super) fetchurl stdenv; };
-  vscode-extensions' = super.callPackage ./vscode-extensions { };
-  base16-unclaimed-schemes = super.callPackage ./base16-unclaimed-schemes { };
-}
