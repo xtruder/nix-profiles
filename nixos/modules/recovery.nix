dcc67c43e1bbe5ac8afc59b499b9859d3baa27f4
Jaka Hudoklin <jaka@x-truder.net>
2020-03-22 16:07:24 +0700

    nixos/modules/recovery: allow passing password and ssh key using env

--- nixos/modules/recovery.nix
+++ nixos/modules/recovery.nix
@@ -5,18 +5,31 @@ with lib;
 let
   cfg = config.system.recovery;
 
+  # tries to get recovery password hash from environment variable
+  recoveryPasswordHash =
+    let passwordHash = builtins.getEnv "recovery_password_hash"; in
+    if passwordHash != ""
+    then passwordHash
+    else "$5$jVNwQm2UlWfF$jdKdRk8JB2cNafMdQOx97EmKdttz6Hr1cNWGaiXfEcC"; # recovery
+
+  # tries to get recovery ssh key from environment variable or use default
+  recoverySshKey =
+    let sshKey = builtins.getEnv "recovery_ssh_key"; in
+    if sshKey != "" then sshKey
+    else null;
+
 in {
   options.system.recovery = {
     passwordHash = mkOption {
       description = "Recovery password";
       type = types.str;
-      default = "$5$jVNwQm2UlWfF$jdKdRk8JB2cNafMdQOx97EmKdttz6Hr1cNWGaiXfEcC";
+      default = recoveryPasswordHash;
     };
 
     sshKey = mkOption {
       description = "SSH recovery public key";
       type = types.nullOr types.str;
-      default = null;
+      default = recoverySshKey;
     };
   };
 
