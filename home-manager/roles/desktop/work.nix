34806b356576bf146fa102a7793899474265cbd7
Jaka Hudoklin <jaka@x-truder.net>
2020-03-28 13:59:32 +0700

    home-manager/roles/desktop: put gnome-apps in work.nix

--- home-manager/roles/desktop/work.nix
+++ home-manager/roles/desktop/work.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 
 let
-
   browser = ["firefox.desktop"];
   textProcessor = ["writer.desktop"];
   spreadsheet = ["impress.desktop"];
   torrent = ["transmission-gtk.desktop"];
+  textEditor = ["nvim.desktop"];
+  imageViewer = ["org.gnome.gThumb.desktop"];
+  archiveTool = ["org.gnome.fileRoller.desktop"];
+  documentViewer = ["org.gnome.Evince.desktop"];
 
 in {
   imports = [
     ../work.nix
-    ./gnome-apps.nix
 
     ../../profiles/firefox
     ../../profiles/chromium.nix
@@ -29,6 +31,13 @@ in {
       "application/vnd.oasis.opendocument.text" = textProcessor;
       "text/csv" = spreadsheet;
       "application/vnd.oasis.opendocument.spreadsheet" = spreadsheet;
+      "text/plain" = textEditor;
+      "image/*" = imageViewer;
+      "application/zip" = archiveTool;
+      "application/rar" = archiveTool;
+      "application/7z" = archiveTool;
+      "application/*tar" = archiveTool;
+      "application/pdf" = documentViewer;
     };
 
     home.packages = with pkgs; [
@@ -45,6 +54,13 @@ in {
       unetbootin
 
       tor-browser-bundle-bin
+
+      # gnome apps
+      gnome3.nautilus
+      gnome3.evince
+      gnome3.file-roller
+      gnome3.gedit
+      gthumb
     ];
   };
 }
