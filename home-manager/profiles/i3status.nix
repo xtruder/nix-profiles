7b736529ba0584ab5a1660ad7c135f0f28c5971f
Jaka Hudoklin <jaka@x-truder.net>
2020-05-12 13:44:42 +0700

    flake: update home-manager

--- home-manager/profiles/i3status.nix
+++ home-manager/profiles/i3status.nix
@@ -6,47 +6,38 @@ with lib;
   config = {
     programs.i3status = {
       enable = true;
+      enableDefault = false;
 
-      order = mkDefault [
-        (mkOrder 500 "online_status")
-        (mkOrder 510 "disk /")
-        (mkOrder 520 "load")
-        (mkOrder 530 "net_rate")
-        (mkOrder 540 "volume master")
-        (mkOrder 511 "battery 0")
-        (mkOrder 550 "tztime local")
-        (mkOrder 551 "tztime pst")
-      ];
+      general = {
+        output_format = "i3bar";
+        colors = true;
+        interval = 5;
+      };
 
-      blocks = {
-        general.opts = {
-          output_format = "i3bar";
-          colors = true;
-          interval = 5;
+      modules = {
+        "online_status" = {
+          position = 1;
         };
 
-        net_rate.opts = {
-          interfaces = "ens3,ens4,wlan0,eth0";
-          all_interfaces = false;
-          si_units = true;
-        };
-
-        load.opts = {
-          format = "↺ %1min";
-        };
-
-        disk_root = {
-          type = "disk";
-          name = "/";
-          opts = {
-            format = "√ %free";
+        load = {
+          position = 2;
+          settings = {
+            format = "↺ %1min";
           };
         };
 
-        volume_master = {
-          type = "volume";
-          name = "master";
-          opts = {
+        net_rate = {
+          position = 3;
+          settings = {
+            interfaces = "ens3,ens4,wlan0,eth0";
+            all_interfaces = false;
+            si_units = true;
+          };
+        };
+
+        "volume master" = {
+          position = 5;
+          settings = {
             format = "♪ %volume";
             device = "default";
             mixer = "Master";
@@ -54,29 +45,25 @@ with lib;
           };
         };
 
-        online = {};
-
-        battery_0 = {
-          type = "battery";
-          name = "0";
-          opts = {
+        "battery all" = {
+          position = 6;
+          settings = {
             format = "%status %percentage %remaining";
             low_threshold = 10;
             last_full_capacity = true;
           };
         };
 
-        tztime_local = {
-          type = "tztime";
-          name = "local";
-          opts.format = "%Y-%m-%d ⌚ %H:%M:%S";
+        "tztime local" = {
+          position = 7;
+          settings = {
+            format = "%Y-%m-%d ⌚ %H:%M:%S";
+          };
         };
 
-        tztime_pst = {
-          type = "tztime";
-          name = "pst";
-
-          opts = {
+        "tztime pst" = {
+          position = 8;
+          settings = {
             format = "PST⌚ %H:%M";
             timezone = "America/Los_Angeles";
           };
