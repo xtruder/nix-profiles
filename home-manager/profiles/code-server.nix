df6e6efcbcf1c8b1297b030d47e8bd087655c83f
Jaka Hudoklin <jaka@x-truder.net>
2020-05-12 22:40:52 +0700

    home-manager/profiles/code-server: reuse settings from vscode

--- home-manager/profiles/code-server.nix
+++ home-manager/profiles/code-server.nix
@@ -1,5 +1,11 @@
-{ config, ... }:
+{ config, lib, ... }:
+
+with lib;
 
 {
-  services.code-server.enable = true;
+  services.code-server = {
+    enable = true;
+    extensions = mkDefault config.programs.vscode.extensions;
+    userSettings = mkDefault config.programs.vscode.userSettings;
+  };
 }
