7add14c125460dfb6732b01987c814260ee88962
Jaka Hudoklin <jaka@x-truder.net>
2020-07-02 14:32:33 +0700

    code-server: set default auth and bindAddr

--- home-manager/profiles/code-server.nix
+++ home-manager/profiles/code-server.nix
@@ -6,6 +6,8 @@ with lib;
   services.code-server = {
     enable = true;
     extensions = mkDefault config.programs.vscode.extensions;
+    auth = mkDefault "none";
+    bindAddr = mkDefault "0.0.0.0:8080";
     userSettings = mkMerge [
       config.programs.vscode.userSettings
       {
